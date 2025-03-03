0e12d99a753e41b1bfc57ca0b8fb5ab7caa8ff99
Evgeniia Granina <evgeniia.granina@iths.se>
2025-03-03 13:54:31 +0100

    Add method DairyApplication

---
+++ src/main/java/org/jakarta/DairyApplication.java
@@ -0,0 +1,9 @@
+package org.jakarta;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class DairyApplication extends Application {
+
+}
