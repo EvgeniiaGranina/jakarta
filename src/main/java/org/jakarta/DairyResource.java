086aea53fb06ae80abc40949be7995292742040d
Evgeniia Granina <evgeniia.granina@iths.se>
2025-03-03 20:43:25 +0100

    Update methods Response och Resource

--- src/main/java/org/jakarta/DairyResource.java
+++ src/main/java/org/jakarta/DairyResource.java
@@ -1,19 +1,42 @@
 package org.jakarta;
 
+import jakarta.inject.Inject;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import lombok.extern.java.Log;
 import org.jakarta.dto.DairyProductResponse;
+import org.jakarta.entity.DairyProduct;
+
+import java.time.LocalDate;
 
 
 @Path("dairies")
 @Log
 public class DairyResource {
 
+    @Inject
+    private Repository repository;
+
+
+
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public DairyProductResponse firstTest(){
-        return new DairyProductResponse("Ost", 123);
+        return new DairyProductResponse("Ost", 123, "Mild", LocalDate.of(2025, 03, 02), "Arla", 1000.30);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public DairyProductResponse create (DairyProduct dairyProduct){
+
+        repository.saveDairyProduct(dairyProduct);
+        return new DairyProductResponse("Ost", 123, "Mild", LocalDate.of(2025, 03, 02), "Arla", 1000.30);
     }
 
 //    private DairyRepository repository;
--- src/main/java/org/jakarta/dto/DairyProductResponse.java
+++ src/main/java/org/jakarta/dto/DairyProductResponse.java
@@ -1,16 +1,27 @@
 package org.jakarta.dto;
 
+import java.time.LocalDate;
+
 public class DairyProductResponse {
 
     private String name;
     private double price;
+    private String description;
+    private LocalDate productionDate;
+    private String brand;
+    private double weight;
 
     public DairyProductResponse() {}
 
 
-    public DairyProductResponse(String name, double price) {
+    public DairyProductResponse(String name, double price, String description, LocalDate productionDate, String brand, double weight) {
         this.name = name;
         this.price = price;
+        this.description = description;
+        this.productionDate = productionDate;
+        this.brand = brand;
+        this.weight = weight;
+
     }
 
     public String getName() {
@@ -27,5 +38,37 @@ public class DairyProductResponse {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(LocalDate productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 }
