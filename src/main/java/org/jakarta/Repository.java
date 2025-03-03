0af41223a49e0ad93d3dcfd7994e575284a545e4
Evgeniia Granina <evgeniia.granina@iths.se>
2025-03-03 20:42:11 +0100

    Add method Repository

---
+++ src/main/java/org/jakarta/Repository.java
@@ -0,0 +1,22 @@
+package org.jakarta;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.jakarta.entity.DairyProduct;
+
+@ApplicationScoped
+public class Repository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Repository(){}
+
+    @Transactional
+    public void saveDairyProduct(DairyProduct dairyProduct){
+        entityManager.persist(dairyProduct);
+    }
+}
