9f64e3e551b0a6426a37c529cea60eb8f63f5abc
Evgeniia Granina <evgeniia.granina@iths.se>
2025-03-05 12:29:51 +0100

    Add record DairyProductDto

---
+++ src/main/java/org/jakarta/dto/DairyProductDto.java
@@ -0,0 +1,20 @@
+package org.jakarta.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * DTO for {@link org.jakarta.entity.DairyProduct}
+ */
+public record DairyProductDto(Long id, @NotBlank(message = "Title is mandatory") String name,
+                              @NotBlank(message = "Description is mandatory") String description,
+                              @NotNull(message = "Production date is mandatory") @PastOrPresent(message = "Production date must be in the past or present") LocalDate productionDate,
+                              @NotBlank(message = "Brand is mandatory") String brand,
+                              @Positive(message = "Price must be positive") double price,
+                              @Positive(message = "Weight must be positive") double weight) implements Serializable {
+}
