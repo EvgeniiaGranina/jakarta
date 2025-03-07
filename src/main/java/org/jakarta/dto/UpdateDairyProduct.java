d6639aab462e47c67b7456b281f164fc600e97c6
Evgeniia Granina <evgeniia.granina@iths.se>
2025-03-07 08:46:36 +0100

    Add validations in UpdateDairyProduct and CreateDairyProduct

--- src/main/java/org/jakarta/dto/UpdateDairyProduct.java
+++ src/main/java/org/jakarta/dto/UpdateDairyProduct.java
@@ -1,6 +1,16 @@
 package org.jakarta.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDate;
 
-public record UpdateDairyProduct(String name, String description, String brand, double price, double weight) {
+public record UpdateDairyProduct(Long id, @NotBlank(message = "Title is mandatory") String name,
+                                 @NotBlank(message = "Description is mandatory") String description,
+                                 @NotNull(message = "Production date is mandatory") @PastOrPresent(message = "Production date must be in the past or present") LocalDate productionDate,
+                                 @NotBlank(message = "Brand is mandatory") String brand,
+                                 @Positive(message = "Price must be positive") double price,
+                                 @Positive(message = "Weight must be positive") double weight) {
 }
